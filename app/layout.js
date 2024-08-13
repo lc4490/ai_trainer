b891e6d3b804b27defc54cec865d207863646bc2
Leo <lc4490@nyu.edu>
2024-08-13 17:53:25 -0400

    name change

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "trainerGPT",
+  title: "AI Personal Trainer",
   description: "An AI assistant that teaches you how to workout.",
 };
 
